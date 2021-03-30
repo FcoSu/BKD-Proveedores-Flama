d6e05561f5966efe43b7458fcc1a3152b3bb996f
Francisco <panchoseur@gmail.com>
2021-03-30 11:41:44 -0300

    first commit

---
+++ src/main/java/com/everis/latam/BKDProveedoresFlama/BkdProveedoresFlamaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.latam.BKDProveedoresFlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkdProveedoresFlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkdProveedoresFlamaApplication.class, args);
+	}
+
+}
